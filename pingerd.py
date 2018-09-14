44d691081c6605515bfc6800d7b1c85670c5ae5a
milnepe <milnepe@gmail.com>
2018-09-14 17:04:56 +0100

    Initial commit

---
+++ pingerd.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+	Pinger Daemon
+"""
+
+import time
+import sys
+while True:
+	print("ping")
+	time.sleep(2)
+
---
+++ pingerd.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Daemon that keeps on pinging
+
+while true
+do
+	echo "ping"
+	sleep 2
+done
+
+exit 0
